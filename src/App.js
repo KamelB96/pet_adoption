0b13fbc6b7709fe13c6d380930bc9dbbb6484a4c
kamel Barakat <kamelbarakat96@yahoo.com>
2023-02-06 19:50:40 -0500

    starting homepage

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pet Adoption</h1>
+      <br>
+      </br>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/dogs">Dogs</a>
+          </li>
+          <li>
+            <a href="/about">About Us</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
