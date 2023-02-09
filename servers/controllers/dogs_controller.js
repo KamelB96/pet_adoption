17eda50c2701eb581c31a998be03ca7dfb3332fe
Veny Cordova <venycordova@gmail.com>
2023-02-08 21:32:51 -0500

    adding a server for the backend

---
+++ servers/controllers/dogs_controller.js
@@ -0,0 +1,81 @@
+const dogs = require('express').Router()
+const db = require('../../models')
+const { Dog } = db
+const { Op } = require('sequelize')
+
+
+dogs.get('/', async (req,res) => {
+    try {
+        const foundDogs = await Dog.findAll({
+            order: [ [ 'age', 'ASC' ] ],
+            where: {
+                name :{[Op.like]: `%${req.query.name ? req.query.name : ''}%`}
+
+}
+        })
+        res.status(200).json(foundDogs)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+    })
+
+
+dogs.get('/:name', async (req,res) => {
+    try {
+        const foundDog = awaitDog.findOne({
+            where: { name: req.params.name }, 
+        })
+        res.status(200).json(foundDog)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+    })
+
+//create a dog
+dogs.post('/api/submit', async (req,res) => {
+    try {
+        const newDog = await Dog.create(req.body)
+        console.log(req.body)
+        res.status(200).json({
+            message: "successfully inerted new dog",
+            data: newDog
+        })
+    } catch (error) {
+        res.status(500).json(error)
+    }
+    })
+
+
+dogs.put('/:id', async (req, res) => {
+    try {
+        const updatedDogs = await Dog.update(req.body, {
+            where: {
+                dog_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully updated ${updatedDogs} dog(s)`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// DELETE A BAND
+dogs.delete('/:id', async (req, res) => {
+    try {
+        const deletedDogs = await Dog.destroy({
+            where: {
+                dog_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully deleted ${deletedDogs} dog(s)`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+//export
+module.exports = dogs;
