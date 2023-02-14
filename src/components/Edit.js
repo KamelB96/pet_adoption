a0067645e234cb800427805973d1b3dc87555984
kamel Barakat <kamelbarakat96@yahoo.com>
2023-02-13 20:25:34 -0500

    minor changes

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,31 +1,40 @@
 import React, { useState } from 'react';
 import axios from "axios";
-import { useParams } from 'react-router-dom';
 
 function Edit (){
-  let id = window.location.href[window.location.href.length - 1]
-  // let { id } = useParams();
-  // console.log(window.location.href)
+  let id = window.location.href.split('/')
+  id = id[id.length -1]
   console.log(id)
-
 
   const [dogs, setDogs] = useState([]);
   const [editingDog, setEditingDog] = useState(null);
+  const [ dogName, setDogName ] = useState('');
+  const [ dogBreed, setDogBreed ] = useState('');
+  const [ dogAge, setDogAge ] = useState(0);
+  const [ dogImage, setDogImage ] = useState('');
+  const [ dogEmail, setDogEmail ] = useState('');
+  
 
   const handleEdit = (dog) => {
     setEditingDog(dog);
   };
 
-  const handleUpdate = async (updatedDog) => {
+  const handleUpdate = async () => {
+    const updated = {
+      dogName,
+      dogBreed,
+      dogAge,
+      dogImage,
+      dogEmail,
+    }
+    
     try {
-      const response = await axios.put(`/api/dogs/${updatedDog.dog_id}`, updatedDog);
-      setDogs(dogs.map(dog => (dog.dog_id === updatedDog.dog_id ? response.data : dog)));
-      setEditingDog(null);
+      const response = await axios.put(`/api/dogs/${updated.dog_id}`, updated);
     } catch (error) {
       console.error(error);
     }
   };
-  
+  console.log(dogName);
   return(
 
     <div className="form">
@@ -33,31 +42,31 @@ function Edit (){
         <form action={`/dogs/${id}`} method="PUT">
         <label htmlFor="name">Name:</label>
         <br></br>
-        <input type="text" id="name" name="name" required></input>
+        <input type="text" id="name" name="name" required onChange={(e) => setDogName(e.target.value)}></input>
         <br></br>
         <hr></hr>
 
         <label htmlFor="breed">Breed:</label><br></br>
-        <input type="text" id="breed" name="breed" required></input>
+        <input type="text" id="breed" name="breed" required onChange={(e) => setDogBreed(e.target.value)}></input>
         <br></br>
         <hr></hr>
         
         <label htmlFor="age">Age:</label><br></br>
-        <input type="number" id="age" name="age" required></input>
+        <input type="number" id="age" name="age" required onChange={(e) => setDogAge(e.target.value)}></input>
         <br></br>
         <hr></hr>
         
         <label htmlFor="image">Image URL:</label><br></br>
-        <input type="text" id="image" name="image" required></input>
+        <input type="text" id="image" name="image" required onChange={(e) => setDogImage(e.target.value)}></input>
         <br></br>
         <hr></hr>
         
         <label htmlFor="email">Email:</label><br></br>
-        <input type="email" id="email" name="email"required></input>
+        <input type="email" id="email" name="email"required onChange={(e) => setDogEmail(e.target.value)}></input>
         <br></br>
         <hr></hr>
         
-        <input type="submit"></input>
+        <button onClick={() => handleUpdate(dogs.dog_id)}>Submit</button>
     </form>
     
     </div>
